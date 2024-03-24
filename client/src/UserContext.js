332c7f8f8bd86dd00e59bfe794998564481443e3
niharikab04 <117061830+niharikab04@users.noreply.github.com>
2024-03-24 10:31:45 +0530

    Update UserContext.js

--- client/src/UserContext.js
+++ client/src/UserContext.js
@@ -7,7 +7,7 @@ export function UserContextProvider({ children }) {
     const [isLoading, setIsLoading] = useState(true); // Add loading state
     
     useEffect(() => {
-        fetch('http://127.0.0.1:5000/profile', {
+        fetch('https://glidethrough-backend.vercel.app/profile', {
             credentials: 'include',
         })
         .then(res => {

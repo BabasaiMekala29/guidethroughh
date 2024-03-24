01ccb9349386ac3d7c87dc9151698d6cb1bade5e
niharikab04 <117061830+niharikab04@users.noreply.github.com>
2024-03-24 11:00:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,11 +9,34 @@ const jwt = require('jsonwebtoken');
 const app = express();
 
 // middleware
-app.use(express.static('public'));
-app.use(cookieParser());
 app.use(cors());
 app.use(cors({credentials:true, origin: 'https://glidethrough-frontend.vercel.app'}));
+app.use(express.static('public'));
+app.use(cookieParser());
 app.use(express.json());
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+
 
 
 const dbURI = process.env.DB_URL;
@@ -24,5 +47,5 @@ mongoose.connect(dbURI)
 // app.get('*', checkUser);
 
 
-
+module.exports = allowCors(handler)
 app.use(webRoutes);

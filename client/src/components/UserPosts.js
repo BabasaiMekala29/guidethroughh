7a2ae2d8e9a0cebc8a51cf4c353525131d705aa2
niharikab04 <117061830+niharikab04@users.noreply.github.com>
2024-03-24 10:40:53 +0530

    Update UserPosts.js

--- client/src/components/UserPosts.js
+++ client/src/components/UserPosts.js
@@ -15,7 +15,7 @@ function UserPosts() {
                 if(id){
 
                 
-                    const response = await fetch(`http://127.0.0.1:5000/user/${id}`);
+                    const response = await fetch(`https://glidethrough-backend.vercel.app/user/${id}`);
                     if (!response.ok) {
                         throw new Error('Failed to fetch posts');
                     }

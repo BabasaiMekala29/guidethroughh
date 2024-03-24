36c579a821b6c1914cc9c1057e0e2f0bc7579624
niharikab04 <117061830+niharikab04@users.noreply.github.com>
2024-03-24 11:49:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,31 +14,31 @@ app.use(cors({credentials:true, origin: 'https://glidethrough-frontend.vercel.ap
 app.use(express.static('public'));
 app.use(cookieParser());
 app.use(express.json());
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', 'https://glidethrough-frontend.vercel.app')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
+// const allowCors = fn => async (req, res) => {
+//   res.setHeader('Access-Control-Allow-Credentials', true)
+//   res.setHeader('Access-Control-Allow-Origin', 'https://glidethrough-frontend.vercel.app')
+//   // another common pattern
+//   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+//   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+//   res.setHeader(
+//     'Access-Control-Allow-Headers',
+//     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+//   )
+//   if (req.method === 'OPTIONS') {
+//     res.status(200).end()
+//     return
+//   }
+//   return await fn(req, res)
+// }
 
-const handler = (req, res) => {
-  if (req.method !== 'OPTIONS') {
-    const d = new Date()
-    res.end(JSON.stringify(d))
-  }
-  // const d = new Date()
-  // res.end(JSON.stringify(d))
-}
+// const handler = (req, res) => {
+//   if (req.method !== 'OPTIONS') {
+//     const d = new Date()
+//     res.end(JSON.stringify(d))
+//   }
+//   // const d = new Date()
+//   // res.end(JSON.stringify(d))
+// }
 
 
 
@@ -51,5 +51,5 @@ mongoose.connect(dbURI)
 // app.get('*', checkUser);
 
 
-module.exports = allowCors(handler)
+// module.exports = allowCors(handler)
 app.use(webRoutes);

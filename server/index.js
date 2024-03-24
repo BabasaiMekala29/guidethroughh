7d6db2caa8303eb98c3c4c02513f3b0dd99d26cb
niharikab04 <117061830+niharikab04@users.noreply.github.com>
2024-03-24 16:10:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,28 +14,7 @@ app.use(cors({credentials:true, origin: 'https://glidethrough-frontend.vercel.ap
 app.use(express.static('public'));
 app.use(cookieParser());
 app.use(express.json());
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', 'https://glidethrough-frontend.vercel.app')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
-}
 
-const handler = (req, res) => {
-    const d = new Date()
-    res.end(JSON.stringify({d}));
-  
-}
 
 
 
@@ -49,5 +28,4 @@ mongoose.connect(dbURI)
 
 // app.get('*', checkUser);
 
-module.exports = allowCors(handler)
 app.use(webRoutes);

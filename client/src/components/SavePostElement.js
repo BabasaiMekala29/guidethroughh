e787e2e1ba23a1baa27e8c3d1d36e4cc1f085c99
niharikab04 <117061830+niharikab04@users.noreply.github.com>
2024-03-24 10:38:53 +0530

    Update SavePostElement.js

--- client/src/components/SavePostElement.js
+++ client/src/components/SavePostElement.js
@@ -22,7 +22,7 @@ function SavePostElement({ post }) {
   const unsavePost = async () => {
     // console.log(id);
     try {
-      const response = await fetch(`http://127.0.0.1:5000/unsavepost/${post._id}`, {
+      const response = await fetch(`https://glidethrough-backend.vercel.app/unsavepost/${post._id}`, {
         method: 'PUT',
         body: JSON.stringify({ id, username: userInfo?.username }),
         headers: { 'Content-Type': 'application/json' },

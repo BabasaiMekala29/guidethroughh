a53c625c13dd88bcbaf3103c22e4ac3e7f9e61bf
niharikab04 <117061830+niharikab04@users.noreply.github.com>
2024-03-31 15:45:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,16 +9,22 @@ const jwt = require('jsonwebtoken');
 const app = express();
 
 // middleware
-app.use(cors());
-// app.use(cors({credentials:true, origin: 'https://glidethrough-frontend.vercel.app'}));
-app.use((req, res, next) => {
+// app.use(cors());
+// // app.use(cors({credentials:true, origin: 'https://glidethrough-frontend.vercel.app'}));
+// app.use((req, res, next) => {
   
-  res.setHeader('Access-Control-Allow-Origin', 'https://glidethrough-frontend.vercel.app'); // Change this to your frontend domain
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials (e.g., cookies)
-  next();
-});
+//   res.setHeader('Access-Control-Allow-Origin', 'https://glidethrough-frontend.vercel.app'); // Change this to your frontend domain
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials (e.g., cookies)
+//   next();
+// });
+app.use(cors({
+  origin: 'https://glidethrough-frontend.vercel.app',
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version', 'Content-Length', 'Content-MD5', 'Content-Type', 'Date', 'X-Api-Version'],
+  credentials: true
+}));
 
 app.use(express.static('public'));
 app.use(cookieParser());

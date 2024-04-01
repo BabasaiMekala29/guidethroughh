d96c737614a1c5d667859fd4a490c47df0bc1c47
niharikab04 <117061830+niharikab04@users.noreply.github.com>
2024-04-01 11:09:11 +0530

    Update HomePage.js

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,27 +2,19 @@ import '../logo1.css';
 import { Button } from '@mui/material'
 import React from 'react'
 import Header from './Header'
-import BackgroundImage from '../images/new.jpg'
-import MobileBgImage from '../images/pbg.jpg'
 function HomePage() {
   return (
     <div style={{ position: 'relative' }}
     >
       <Header />
-      <div id='mainContainer'
-        // style={{
-        //   backgroundImage: `url(${BackgroundImage})`,
-        //   backgroundSize: 'cover',
-        //   backgroundRepeat: 'no-repeat',
-        //   height: '100vh',
-        // }}
-      >
+      <div id='mainContainer'>
         
           <div className="animated-text">
             <div>G<span></span>
               <div>Through </div>
             </div>
           </div>
+          <p id='description'>Feeling lost, confused, or uncertain? <br />Don't worry, we've gotcha. Stop running circles;<br /> dive right in. Let the community guide you through. <br />Explore curated categories, uncover valuable insights and let it all glide through effortlessly. </p>
           <Button id='exploreBtn' href='/category' variant="contained" size='large'>Explore</Button>
         </div>
       </div>
